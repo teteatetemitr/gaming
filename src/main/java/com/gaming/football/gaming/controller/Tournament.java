d2073e3085523aa89c3f6c379ee8a79b05e84fb3
Sghose <sghose@aig.com>
2018-11-13 17:24:05 +0530

    Request mapping added

--- src/main/java/com/gaming/football/gaming/controller/Tournament.java
+++ src/main/java/com/gaming/football/gaming/controller/Tournament.java
@@ -1,8 +1,10 @@
 package com.gaming.football.gaming.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tournament")
 public class Tournament {
 
 }
